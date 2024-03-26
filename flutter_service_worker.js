c93b0b71f57002a438c1c2cddb24eebc675a041f
RajwolChapagain <RajwolChapagain@users.noreply.github.com>
2024-03-26 22:14:24 +0000

    deploy: d317048257ce0b88f677a3e3aed81ffa47e6a9f4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "55ede96cdbe37eb6ef46223055ac4fd7",
-"/": "55ede96cdbe37eb6ef46223055ac4fd7",
+const RESOURCES = {"index.html": "4214e88488d96ea6d36fb133354f2006",
+"/": "4214e88488d96ea6d36fb133354f2006",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
