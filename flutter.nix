9dbb0d48ce8b21176a4005261da78a7382b985d8
Rajwol Chapagain <rajwolchapagain@gmail.com>
2024-04-26 18:57:01 -0400

    Add flutter.nix

---
+++ flutter.nix
@@ -0,0 +1,20 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    flutter
+    which
+    bash
+    file
+    coreutils
+    curl
+    git
+    unzip
+    xz
+    zip
+    libGLU
+  ];
+}
